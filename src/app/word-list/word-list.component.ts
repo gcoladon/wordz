b20c9cb6ee5ce4961ad5030024355291e79336fa
gcoladonato3 <gcoladonato3@gatech.edu>
2019-11-22 12:22:02 -0800

    getting the word list to work

--- src/app/word-list/word-list.component.ts
+++ src/app/word-list/word-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { words } from '../words';
+
 @Component({
-  selector: 'app-word-list',
-  templateUrl: './word-list.component.html',
-  styleUrls: ['./word-list.component.css']
+    selector: 'app-word-list',
+    templateUrl: './word-list.component.html',
+    styleUrls: ['./word-list.component.css']
 })
 export class WordListComponent implements OnInit {
+    words = words
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
