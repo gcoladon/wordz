30642ceb70ebd0c9da2ec417ccd476ce7f4df132
gcoladonato3 <gcoladonato3@gatech.edu>
2019-11-22 15:50:57 -0800

    adding hearse

--- src/app/words.ts
+++ src/app/words.ts
@@ -10,5 +10,11 @@ export const words = [
     book_id: 1,
     word: 'cremling',
     def: 'a group of tiny scavenger crustacean-like creatures found on Roshar. The term cremling is often used to degrade and insult. They are common in nearly every freshwater habitat, where crem is found.'
+  },
+  {
+    id: 3,
+    book_id: 4,
+    word: 'hearse',
+    def: 'a vehicle for conveying a dead person to the place of burial.'
   }
 ];
